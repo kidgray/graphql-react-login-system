8180c68b96f1f6daca71f9931f7d92ec9b57b34e
Micah Allauca <micah.allauca@gmail.com>
2020-08-02 20:56:30 -0400

    Why does VSCode keep changing className to class in my JSX elements

--- src/client/pages/not-found/not-found.component.jsx
+++ src/client/pages/not-found/not-found.component.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
         <div className="jumbotron">
             <h1 className="display-1 not-found-header">404: NOT FOUND</h1>
             <hr />
-            <p class="lead not-found-msg">The page you're looking for was not found.</p>
+            <p className="lead not-found-msg">The page you're looking for was not found.</p>
 
             <button class="btn btn-primary not-found-button"> Home </button>
         </div>
